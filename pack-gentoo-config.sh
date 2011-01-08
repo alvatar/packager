17b5dba5faef4fa46fda3bddcd84b89fb5ceb8b8
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2011-01-08 16:25:38 +0100

    added simpler packer

---
+++ pack-gentoo-config.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+
+IN_USER_FILES="
+.Xmodmaprc
+.Xdefaults
+.bashrc
+.bash_profile
+.bin
+.blackhole/pkglist
+.config/uzbl
+.emacs
+.emacs.d
+.gambcini
+.gtkrc-2.0
+.irssi
+.lftp
+.lftprc
+.moc/config
+.prompt
+.prompt_config
+.prompt_functions
+.rtorrent.rc
+.themes
+.vim
+.vimrc
+.xbindkeysrc
+.xinitrc
+.xmonad/xmonad.hs
+"
+IN_SYSTEM_FILES="
+/boot/grub/grub.conf
+/etc/fstab
+/etc/hosts
+/etc/locale.gen
+/etc/make.conf
+/etc/rc.conf
+/etc/X11/xorg.conf
+/etc/bitlbee/bitlbee.conf
+/etc/conf.d/consolefont
+/etc/conf.d/hwclock
+/etc/conf.d/keymaps
+/etc/conf.d/net
+/etc/portage/package.keywords
+/etc/portage/package.use
+/root/.ecryptfsrc
+/usr/src/linux/.config
+/var/lib/bitlbee/alvatar.xml
+"
+
+declare -x AM_I_ROOT=false
+if [ `id -u` != "0" ]; then
+	echo -e "\nError: You must be root to make a backup of certain files\n" >&2
+	exit 192
+else
+    AM_I_ROOT=true
+fi
+
+if [ -d "user_files_tmp" ]; then
+    echo -e "\nError: the directory user_files_tmp already existsi\n" >&2
+    exit 192
+fi
+
+mkdir user_files_tmp
+cp --parents -R $IN_USER_FILES user_files_tmp
+
+IN_FILES="
+user_files_tmp
+$IN_SYSTEM_FILES
+"
+
+OUT_FILENAME="gentoo_system_config_"`(date +%g_%m_%d)`".tar.bz2"
+
+tar cjf $OUT_FILENAME $IN_FILES
+
+if [ -d "user_files_tmp" ]; then
+    rm -R user_files_tmp
+fi
